ff91534bf5518c863bfad42507f4af6a18ab58a0
Mulubi <mulubiwarren@gmail.com>
2022-12-22 08:25:47 +0000

    Changed some code4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,36 +5,29 @@
 # host=['54.157.156.170' , '3.90.85.138']
 # user='ubuntu'
 
+# Connect to the host and update the package lists
+sudo apt-get update
+# Install nginx
+sudo apt-get install -y nginx
+
 # Set the required directories
-data='/data/'
-web_static='/data/web_static/'
-releases='/data/web_static/releases/'
-shared='/data/web_static/shared/'
-releases_test='/data/web_static/releases/test/'
-symbolic_link='/data/web_static/current'
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/current
+
 # Create a fake HTML file
 html_file='/data/web_static/releases/test/index.html'
 
-# Connect to the host and update the package lists
-sudo apt-get update
-# Install nginx 
-sudo apt-get install -y nginx
-
-#Create the required folders if they do not exist
-if [ ! -d $releases_test ]; then mkdir -p
-$releases_test; fi
-if [ ! -d $shared ]; then mkdir -p
-$shared; fi
-if [ ! -d $symbolic_link ]; then mkdir -p
-$symbolic_link; fi
-
 # Insert simple content into the html file
 echo "Warren Mulubi Tech Guy" > html_file
-ln -sf $releases_test $symbolic_link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Set the ownership and permission of the data file
-chown -R ubuntu $data
-chgrp -R ubuntu $data
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 # Set the Nginx configuration file
 nginx_config="/etc/nginx/sites-available/default"
@@ -58,7 +51,7 @@ printf %s "server {
       root /var/www/html;
       internal;
     }
-}" > $nginx_config
+}" > nginx_config
 
 # Restart Nginx
 service nginx restart
