cbaa4c3e4a6403be84c0239ff05b71bc35ce7440
Mulubi <mulubiwarren@gmail.com>
2022-12-22 13:39:59 +0000

    Pack web static2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-# This is a fabric script that generates a .tgz archive from
-# the contents of the web_static folder of your repo and
-# stores it in the versions folder:
-from fabric.api import local, lcd, env
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    # Create the versions directory if it does not exist
-    local("if [ ! -d versions ]; then mkdir versions; fi")
-
-    # Get the current date and time
+    """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
-    
-    # Create the archive name
-    archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-								 dt.month,
-								 dt.day,
-								 dt.hour,
-								 dt.minute,
-								 dt.second)
-    # Change to the web_static directory
-    with lcd("web_static"):
-        # Create the tar file
-        local("tar -cvzf {} web_static".format(archive_name))
-
-    # Return the path to the archive
-    return archive_name
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
